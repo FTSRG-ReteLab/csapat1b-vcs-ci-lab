1816b4c4e23f186e1dc22d4057f1cf7e0d98f940
gnanetta <40520810gnanetta@users.noreply.github.com>
2019-02-04 08:06:07 -0800

    new junit test

---
+++ train-system/src/test/java/hu/bme/mit/train/user/TrainUserImplTest.java
@@ -0,0 +1,12 @@
+package hu.bme.mit.train.user;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TrainUserImplTest {
+
+    @Test
+    public void getAlarmFlag() {
+    }
+}
